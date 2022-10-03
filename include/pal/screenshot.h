4d0a8f5029b001455dc3d8c9b456ea6c1d625409
Andrew <apalrd@gmail.com>
2022-10-02 22:52:33 -0400

    Create header

---
+++ include/pal/screenshot.h
@@ -0,0 +1,20 @@
+/* Screenshot routine for PROS V5
+ * Copyright (c) 2022 Andrew Palardy
+ * This code is subject to the BSD 2-clause 'Simplified' license
+ * See the LICENSE file for complete terms
+ */
+#ifndef _SCREENSHOT_H_
+#define _SCREENSHOT_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* Routine to take a screenshot */
+void screenshot(const char * fname);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* _SCREENSHOT_H_ */
